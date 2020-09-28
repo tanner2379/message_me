21fdfb5f3bdb3c732155b9aab12c33f7905c6895
Tanner Sigel <tanner2379@live.com>
2020-09-28 11:41:30 -0700

    Implemented ActionCable for realtime messaging

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,23 @@
+class MessagesController < ApplicationController
+before_action :require_user
+
+
+  def create
+    message = current_user.messages.build(message_params)
+    if message.save
+      ActionCable.server.broadcast "chatroom_channel",
+                                    mod_message: message_render(message)
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
+  end
+
+  def message_render(obj)
+    render(partial: 'message', locals: {message: obj})
+  end
+
+end
