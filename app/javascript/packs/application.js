a5a43a21a351df062ec183db06bffe7e05c5a8c7
Tanner Sigel <tanner2379@live.com>
2020-09-28 11:52:54 -0700

    made message box scroll to bottom with new messages and on page load

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,10 +19,18 @@ require("semantic-ui-sass")
 // Loads all Semantic javascripts
 //= require jquery
 
+//scrolls to the bottom every time this message is called,
+//scrolls to the 0th height of messages
+scroll_bottom = function() {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
 $(document).on("turbolinks:load", function() {
   $(".ui.dropdown").dropdown();
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
-  })
-;
+  });
+  scroll_bottom();
 })
